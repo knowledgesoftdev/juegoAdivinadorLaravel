449f2be77a807be15081e53c2787c4af5f18a5d7
Brayan Trujillo <brayantrujillonieto@gmail.com>
2023-04-20 23:37:32 -0500

    Laravel -  Cambiando el Home / JuegpAdivinador Version 1.3

--- README.md
+++ README.md
@@ -20,6 +20,7 @@
 - Corremos la aplicación: php artisan serve
 
 ## Migraciones
+
 - Crearemos la base de datos que llevara de nombre: juegoAdivinador.
 
 - Una vez creado lo colocamos en la conexion que se encontrara en el env.
@@ -28,8 +29,22 @@
 
 - Colocamos unos campos en la migracion creada:
 
-	[![Screenshot-1.png](https://i.postimg.cc/B6kMBhWV/Screenshot-1.png)](https://postimg.cc/LYktH38t)
+[![Screenshot-1.png](https://i.postimg.cc/ryhn6LkW/Screenshot-1.png)](https://postimg.cc/0rwdmFr2)
 
 - Luego lo migramos: php artisan migrate
+
+## Modificando el home que trae por defecto por el de AdminLte
+
+- Primero corremos nuestra aplicación: php artisan serve
+
+- Tambien para los estilos: npm run dev
+
+- El codigo que cambiaremos en resources->views->home.blade.php:
+
+[![fonto1.png](https://i.postimg.cc/sxds5Qc1/fonto1.png)](https://postimg.cc/YG35HSTw)
+
+- Luego vamos config->adminlte.php en la linea 292 'menu' reemplazamos por este código:
+
+[![foto2.png](https://i.postimg.cc/JzydZ1TB/foto2.png)](https://postimg.cc/DSTgKK32)
 
 
--- config/adminlte.php
+++ config/adminlte.php
@@ -291,35 +291,19 @@ return [
 
     'menu' => [
         // Navbar items:
-        [
-            'type'         => 'navbar-search',
-            'text'         => 'search',
-            'topnav_right' => true,
-        ],
-        [
-            'type'         => 'fullscreen-widget',
-            'topnav_right' => true,
-        ],
+       
 
         // Sidebar items:
-        [
-            'type' => 'sidebar-menu-search',
-            'text' => 'search',
+        ['header' => 'Nuestros Juegos'],
+        [   
+            'text' => 'Adivina el número',
+            'url'  => 'admin/settings',
+            'icon' => 'fas fa-sharp fa-solid fa-hashtag',
         ],
-        [
-            'text' => 'blog',
-            'url'  => 'admin/blog',
-            'can'  => 'manage-blog',
-        ],
-        [
-            'text'        => 'pages',
-            'url'         => 'admin/pages',
-            'icon'        => 'far fa-fw fa-file',
-            'label'       => 4,
-            'label_color' => 'success',
-        ],
-        ['header' => 'account_settings'],
-        [
+
+
+        ['header' => 'Configuraciones de Usuario'],
+        [   
             'text' => 'profile',
             'url'  => 'admin/settings',
             'icon' => 'fas fa-fw fa-user',
@@ -328,61 +312,7 @@ return [
             'text' => 'change_password',
             'url'  => 'admin/settings',
             'icon' => 'fas fa-fw fa-lock',
-        ],
-        [
-            'text'    => 'multilevel',
-            'icon'    => 'fas fa-fw fa-share',
-            'submenu' => [
-                [
-                    'text' => 'level_one',
-                    'url'  => '#',
-                ],
-                [
-                    'text'    => 'level_one',
-                    'url'     => '#',
-                    'submenu' => [
-                        [
-                            'text' => 'level_two',
-                            'url'  => '#',
-                        ],
-                        [
-                            'text'    => 'level_two',
-                            'url'     => '#',
-                            'submenu' => [
-                                [
-                                    'text' => 'level_three',
-                                    'url'  => '#',
-                                ],
-                                [
-                                    'text' => 'level_three',
-                                    'url'  => '#',
-                                ],
-                            ],
-                        ],
-                    ],
-                ],
-                [
-                    'text' => 'level_one',
-                    'url'  => '#',
-                ],
-            ],
-        ],
-        ['header' => 'labels'],
-        [
-            'text'       => 'important',
-            'icon_color' => 'red',
-            'url'        => '#',
-        ],
-        [
-            'text'       => 'warning',
-            'icon_color' => 'yellow',
-            'url'        => '#',
-        ],
-        [
-            'text'       => 'information',
-            'icon_color' => 'cyan',
-            'url'        => '#',
-        ],
+        ],       
     ],
 
     /*
